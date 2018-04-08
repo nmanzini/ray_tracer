84787403bbc018bd665f28829889116b54160701
Nicola MANZINI <nmanzini@e1r8p13.42.fr>
2018-04-09 01:07:53 +0200

    almost normed

--- src/input.c
+++ src/input.c
@@ -6,14 +6,14 @@
 /*   By: nmanzini <nmanzini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/05 19:40:05 by nmanzini          #+#    #+#             */
-/*   Updated: 2018/04/08 23:52:27 by nmanzini         ###   ########.fr       */
+/*   Updated: 2018/04/09 01:07:26 by nmanzini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RTv1.h"
 
-void free_list_str(char **list)
-{	
+void	free_list_str(char **list)
+{
 	int i;
 
 	i = 0;
@@ -25,21 +25,21 @@ void free_list_str(char **list)
 	free(list);
 }
 
-int	check_file(char *path)
+int		check_file(char *path)
 {
 	int		fd;
 	char	*line;
 	int		obj_n;
 
 	obj_n = 0;
-
 	fd = open(path, O_RDONLY);
 	ft_putendl("OPENED");
 	if (fd == -1)
 		return (0);
 	while (get_next_line(fd, &line) > 0)
 	{
-		if (line[0] == 's' || line[0] == 'o' || line[0] == 'y' ||line[0] == 'p')
+		if (line[0] == 's' || line[0] == 'o' ||
+		line[0] == 'y' || line[0] == 'p')
 			obj_n += 1;
 		free(line);
 	}
@@ -50,25 +50,21 @@ int	check_file(char *path)
 	return (obj_n);
 }
 
-int 	write_obj(t_data *dt, int fd, char *line, int *i_obj)
+int		write_obj(t_data *dt, int fd, char *line, int *i_obj)
 {
-	int i;
-	char *line2;
-	char **list;
-	int j;
-
+	int		i;
+	char	*line2;
+	char	**list;
 
 	i = 0;
 	if (line[0] == 's')
 	{
 		if (line[2] == '0')
-			return(0);
+			return (0);
 		*i_obj = *i_obj + 1;
-
 		dt->ob[*i_obj].type = 's';
 		get_next_line(fd, &line2);
 		list = ft_strsplit(line2, ',');
-
 		while (i < 4)
 		{
 			dt->ob[*i_obj].p[i] = ft_atoi(list[i]);
@@ -76,7 +72,6 @@ int 	write_obj(t_data *dt, int fd, char *line, int *i_obj)
 		}
 		free(line2);
 		free_list_str(list);
-
 		get_next_line(fd, &line2);
 		list = ft_strsplit(line2, ',');
 		i = 0;
@@ -91,22 +86,20 @@ int 	write_obj(t_data *dt, int fd, char *line, int *i_obj)
 	else if (line[0] == 'o')
 	{
 		if (line[2] == '0')
-			return(0);
+			return (0);
 		*i_obj = *i_obj + 1;
 		dt->ob[*i_obj].type = 'o';
-		get_next_line(fd,&line2);
+		get_next_line(fd, &line2);
 		list = ft_strsplit(line2, ',');
-
 		while (i < 3)
 		{
-			dt->ob[*i_obj].vp.p[i] = ft_atoi(list[i]); 
-			dt->ob[*i_obj].vp.v[i] = ft_atoi(list[i + 3]); 
+			dt->ob[*i_obj].vp.p[i] = ft_atoi(list[i]);
+			dt->ob[*i_obj].vp.v[i] = ft_atoi(list[i + 3]);
 			i++;
 		}
-		normalize (dt->ob[*i_obj].vp.v);
+		normalize(dt->ob[*i_obj].vp.v);
 		free(line2);
 		free_list_str(list);
-
 		get_next_line(fd, &line2);
 		list = ft_strsplit(line2, ',');
 		i = 0;
@@ -121,22 +114,20 @@ int 	write_obj(t_data *dt, int fd, char *line, int *i_obj)
 	else if (line[0] == 'y')
 	{
 		if (line[2] == '0')
-			return(0);
+			return (0);
 		*i_obj = *i_obj + 1;
 		dt->ob[*i_obj].type = 'y';
-		get_next_line(fd,&line2);
+		get_next_line(fd, &line2);
 		list = ft_strsplit(line2, ',');
-
 		while (i < 3)
 		{
-			dt->ob[*i_obj].vp.p[i] = ft_atoi(list[i]); 
-			dt->ob[*i_obj].vp.v[i] = ft_atoi(list[i + 3]); 
+			dt->ob[*i_obj].vp.p[i] = ft_atoi(list[i]);
+			dt->ob[*i_obj].vp.v[i] = ft_atoi(list[i + 3]);
 			i++;
 		}
-		normalize (dt->ob[*i_obj].vp.v);
+		normalize(dt->ob[*i_obj].vp.v);
 		free(line2);
 		free_list_str(list);
-		
 		get_next_line(fd, &line2);
 		list = ft_strsplit(line2, ',');
 		i = 0;
@@ -151,21 +142,19 @@ int 	write_obj(t_data *dt, int fd, char *line, int *i_obj)
 	else if (line[0] == 'p')
 	{
 		if (line[2] == '0')
-			return(0);
+			return (0);
 		*i_obj = *i_obj + 1;
 		dt->ob[*i_obj].type = 'p';
-		get_next_line(fd,&line2);
+		get_next_line(fd, &line2);
 		list = ft_strsplit(line2, ',');
-
 		while (i < 4)
 		{
-			dt->ob[*i_obj].p[i] = ft_atoi(list[i]); 
+			dt->ob[*i_obj].p[i] = ft_atoi(list[i]);
 			i++;
 		}
-		normalize (dt->ob[*i_obj].p);
+		normalize(dt->ob[*i_obj].p);
 		free(line2);
 		free_list_str(list);
-		
 		get_next_line(fd, &line2);
 		list = ft_strsplit(line2, ',');
 		i = 0;
@@ -180,10 +169,9 @@ int 	write_obj(t_data *dt, int fd, char *line, int *i_obj)
 	else if (line[0] == 'c')
 	{
 		if (line[2] == '0')
-			return(0);
-		get_next_line(fd,&line2);
+			return (0);
+		get_next_line(fd, &line2);
 		list = ft_strsplit(line2, ',');
-
 		while (i < 3)
 		{
 			dt->ca->cam_p[i] = ft_atoi(list[i]);
@@ -196,10 +184,9 @@ int 	write_obj(t_data *dt, int fd, char *line, int *i_obj)
 	else if (line[0] == 'l')
 	{
 		if (line[2] == '0')
-			return(0);
-		get_next_line(fd,&line2);
+			return (0);
+		get_next_line(fd, &line2);
 		list = ft_strsplit(line2, ',');
-
 		while (i < 3)
 		{
 			dt->px->lig->p[i] = ft_atoi(list[i]);
@@ -208,59 +195,49 @@ int 	write_obj(t_data *dt, int fd, char *line, int *i_obj)
 		free(line2);
 		free_list_str(list);
 	}
-
 	return (0);
 }
 
-int loop_trough(char *file_path, t_data *dt)
+int		loop_trough(char *file_path, t_data *dt)
 {
 	int		fd;
 	char	*line;
 	int		i_obj;
 
 	i_obj = -1;
-
 	fd = open(file_path, O_RDONLY);
 	if (fd == -1)
 		return (-1);
-	while (get_next_line(fd, &line) > 0 )
-	{	
+	while (get_next_line(fd, &line) > 0)
+	{
 		if (line[0] != '#' && line[0] != 0 && line[0] != 10)
 		{
-			write_obj(dt,fd,line,&i_obj);
+			write_obj(dt, fd, line, &i_obj);
 		}
 		free(line);
 	}
 	free(line);
-	
 	if (close(fd) == -1)
 		return (-1);
 	return (0);
 }
 
-int read_file(char *path, t_data *dt, int obj_num)
+int		read_file(char *path, t_data *dt, int obj_num)
 {
-	dt->ob  = (t_obj*)malloc(sizeof(t_obj) * (obj_num + 1));
+	dt->ob = (t_obj*)malloc(sizeof(t_obj) * (obj_num + 1));
 	dt->obj_num = obj_num;
-
 	dt->ob[obj_num].type = 'n';
-
 	ft_putnbr(obj_num);
 	ft_putstr(" objs allocated\n");
-	
 	loop_trough(path, dt);
-
 	return (0);
-
 }
 
-
-int	get_input(t_data *dt, int ac, char **av)
+int		get_input(t_data *dt, int ac, char **av)
 {
 	int obj_num;
 
 	obj_num = 0;
-
 	if (ac != 2)
 	{
 		ft_putstr("usage: ./RTv1 scene\n");
@@ -277,7 +254,6 @@ int	get_input(t_data *dt, int ac, char **av)
 		ft_putstr("Reading\n");
 		read_file(av[1], dt, obj_num);
 	}
-
 	ft_putstr(" loaded.\n");
 	return (0);
 }
