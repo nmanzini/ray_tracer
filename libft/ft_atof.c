63463e7901d3975fd6e3974a4431bf9c6dd696dd
Nicola MANZINI <nmanzini@e1r8p13.42.fr>
2018-04-10 21:58:37 +0200

    fixed libft

---
+++ libft/ft_atof.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atof.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmanzini <nmanzini@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/06 21:07:06 by nmanzini          #+#    #+#             */
+/*   Updated: 2018/04/08 16:20:45 by nmanzini         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_isspace(char c)
+{
+	if (c == '\t' || c == '\n' || c == '\r')
+		return (1);
+	else if (c == '\f' || c == '\v' || c == ' ')
+		return (1);
+	else
+		return (0);
+}
+
+float		ft_atof(char *str)
+{
+	float	res;
+	float	fac;
+	int		seen;
+
+	fac = 1;
+	res = 0;
+	while (ft_isspace(*str))
+		str++;
+	if (*str == '-')
+		fac = -1;
+	if (*str == '+' || *str == '-')
+		str++;
+	while ((*str >= '0' && *str <= '9') || *str == '.')
+	{
+		if (*str == '.')
+		{
+			seen = 1;
+			str++;
+		}
+		if (seen)
+			fac /= 10;
+		res = (res * 10) + (*str++ - '0');
+	}
+	return (fac * res);
+}
